557188ab8517b693db1187ca7011d8f02d619087
BongaDigiLink <bonga@digilink.africa>
2023-04-26 11:33:32 +0200

    edit routing components.

--- AngularProject_UMS/src/app/app.module.ts
+++ AngularProject_UMS/src/app/app.module.ts
@@ -1,22 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//Added
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LandingComponent } from './landing/landing.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
+import { HomeComponent } from './home/home.component';
 
-@NgModule({
-  declarations: [
+@NgModule(
+  {
+  declarations: 
+  [
     AppComponent,
     SignInComponent,
     SignUpComponent,
     LandingComponent,
-    UserDetailsComponent
+    UserDetailsComponent,
+    HomeComponent
   ],
-  imports: [
-    BrowserModule
+  imports: 
+  [
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', redirectTo : 'home', pathMatch: 'full'},
+      {path: 'signin', component: SignInComponent},
+      {path: 'signup', component: SignUpComponent},
+      {path: 'user-details', component: UserDetailsComponent},
+      {path: 'landing', component: LandingComponent},
+      {path: 'home', component: HomeComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
