1cdc029824bd9a37a92a47d115d65644b59ff6ee
BongaDigiLink <bonga@digilink.africa>
2023-04-28 12:25:43 +0200

    crud operations services from repository instance

--- ums_backend/src/main/java/services/InternService.java
+++ ums_backend/src/main/java/services/InternService.java
@@ -1,8 +1,37 @@
 package services;
 
+import models.interns.Intern;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import repository.InternRepository;
+
+import java.util.List;
 
 @Component
 public class InternService
 {
+    private final InternRepository internRepository;
+
+    @Autowired
+    public InternService(InternRepository internRepository)
+    {
+        this.internRepository = internRepository;
+    }
+
+    // ---- Get Paths ----
+    public List<Intern> getAllInterns()
+    {
+        return internRepository.findAll();
+    }
+
+    public Intern getInternById(Integer internId)
+    {
+        return internRepository.getById(internId);
+    }
+
+    // ---- Post Paths ----
+    public void registerIntern(Intern intern)
+    {
+        internRepository.save(intern);
+    }
 }
--- ums_backend/src/main/java/services/MentorService.java
+++ ums_backend/src/main/java/services/MentorService.java
@@ -1,9 +1,17 @@
 package services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import repository.MentorRepository;
 
 @Component
 public class MentorService
 {
+    private final MentorRepository mentorRepository;
 
+    @Autowired
+    public MentorService(MentorRepository mentorRepository)
+    {
+        this.mentorRepository = mentorRepository;
+    }
 }
