913ee3d9219f5ea679a01e44b41f5ea4d5fb8d0e
BongaDigiLink <bonga@digilink.africa>
2023-04-28 14:50:01 +0200

    h2 db annotations

--- ums_backend/src/main/java/models/interns/Intern.java
+++ ums_backend/src/main/java/models/interns/Intern.java
@@ -1,7 +1,16 @@
 package models.interns;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "interns")
 public class Intern
 {
+    @Id
+    @GeneratedValue
     private Integer id;
     private String name;
     private String surname;
--- ums_backend/src/main/java/models/mentors/Mentor.java
+++ ums_backend/src/main/java/models/mentors/Mentor.java
@@ -1,7 +1,16 @@
 package models.mentors;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table( name="Mentors" )
 public class Mentor
 {
+    @Id
+    @GeneratedValue
     private Integer id;
     private String name;
     private String surname;
