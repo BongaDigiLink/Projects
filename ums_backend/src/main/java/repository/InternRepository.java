e23a0e82e25a859d6de1595fafc0e698dc37bc10
BongaDigiLink <bonga@digilink.africa>
2023-04-28 12:24:53 +0200

    crud operations for register intern. getById for login.

--- ums_backend/src/main/java/repository/InternRepository.java
+++ ums_backend/src/main/java/repository/InternRepository.java
@@ -2,7 +2,9 @@ package repository;
 
 import models.interns.Intern;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InternRepository extends JpaRepository<Intern, Integer>
 {
 
--- ums_backend/src/main/java/repository/MentorRepository.java
+++ ums_backend/src/main/java/repository/MentorRepository.java
@@ -2,7 +2,9 @@ package repository;
 
 import models.mentors.Mentor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MentorRepository extends JpaRepository<Mentor, Integer>
 {
 }
